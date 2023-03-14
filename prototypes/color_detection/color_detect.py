2b0d69e2b9ba4d6034ab25230e6356fffd8eaa16
Sheharyar Ali <sheharyarali04052000@gmail.com>
2023-03-14 21:54:09 +0100

    Finished KMeans clustering

--- prototypes/color_detection/color_detect.py
+++ prototypes/color_detection/color_detect.py
@@ -10,11 +10,15 @@ def RGB_2_YUV(R,G,B):
     U = (4 * (3385827488 + 13225875 * B - 8762500 * G - 4463375 * R))/105807109
     V = -(4 *(-3385827488 + 2150875 *B + 11075000* G - 13225875* R))/105807109
     return Y,U,V
-def YUV_2_RGB(Y,U,V):
+def YUV_2_RGB(coord):
+    Y = coord[0]
+    U = coord[1]
+    V = coord[2]
     R = Y + 1.402 * (V - 128)
     G = Y - 0.34414 * (U - 128) - 0.71414 * (V - 128)
     B = Y + 1.772 * (U - 128)
-    return R,G,B
+    out = [int(R),int(G),int(B)]
+    return out
 
 #for green we need very low U and V
 y_range=[50,200]
@@ -47,25 +51,51 @@ for i in range(0,len(Filtered)):
 # Filtered = Filtered.transpose()
 plt.figure()
 RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-plt.imshow(YUV)
+plt.imshow(RGB)
 plt.title('Original image')
 
 plt.figure()
 plt.imshow(Filtered)
 plt.title('Filtered image')
 
-cluster = KMeans(n_clusters=3)
-X = YUV.reshape(-1,1)
+
+## Clustering
+cluster = KMeans(n_clusters=6)
+# cluster_db_scan = DBSCAN(eps=10,min_samples=100)
+X=[]
+
+for x in YUV:
+    for y in x:
+        X.append(y)
+X = np.array(X)
 
 kmeans = cluster.fit(X)
-lab = kmeans.labels_
-img_clusters = 128/(lab.max() -lab.min()) * lab
-img_clusters = img_clusters.reshape(YUV.shape)
+img_clusters = kmeans.labels_
 centers = cluster.cluster_centers_
-print(centers)
+# img_clusters = 1/(lab.max() -lab.min()) * lab
+# print(img_clusters)
+# print(len(img_clusters),len(X))
+# exit()
+# img_clusters = img_clusters.reshape(YUV.shape)
+for i in range(0,len(img_clusters)):
+    X[i] = centers[img_clusters[i]]
+X = X.reshape(YUV.shape)
+
+
+## Colour change (not needed on drone)
+X_colour = []
+
+for x in X:
+    for y in x:
+        X_colour.append(YUV_2_RGB(y))
+X_colour=np.array(X_colour)
+X_colour = X_colour.reshape(X.shape)
+
+for i in centers:
+    print("cluster ", i, "RGB: ", YUV_2_RGB(i))
 
 plt.figure()
-plt.imshow(img_clusters)
+plt.imshow(X_colour)
 plt.title('K_Means cluster image')
 
 
